222c529f0464b03996dd44c60d1fefa10f868c1a
naveen199201 <naveen199201@gmail.com>
2023-10-25 19:27:16 +0530

    frontend and backend

---
+++ E-Commerce-Frontend/src/pages/Home.js
@@ -0,0 +1,15 @@
+import { Navbar, Product, Footer } from "../components";
+
+function Home() {
+  return (
+    <>
+     <div>
+       <Navbar />
+       <Product />
+       <Footer />
+       </div> 
+    </>
+  )
+}
+
+export default Home
